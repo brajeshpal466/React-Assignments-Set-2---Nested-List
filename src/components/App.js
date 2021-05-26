f3f386c60a329ba1cf018df4790fc9235527d165
brajeshpal466 <61057420+brajeshpal466@users.noreply.github.com>
2021-05-26 18:00:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -154,8 +154,49 @@ const states = [
   },
 ];
 
+function Town(props) {
+  return (
+    <div>
+      <div id={props.id}>{props.name}</div>
+    </div>
+  )
+}
+
+
+function City(props){
+  const [getcity, setcity] = useState(false);
+  const cityclick = (e)=>{
+    setcity(!getcity);
+     e.stopPropagation();
+  }
+    return(
+      <>
+      <div onClick={cityclick} id={props.id}>{props.name}</div>
+      {getcity && props.city.map( (tw,index) => <Town id={`town${index+1}`} name={tw.name}></Town>)}
+      </>
+    )
+}
+
+function States(props){
+ const [getcity, setcity] = useState(false);
+     const stateclick = (e)=>{
+          setcity(!getcity);
+          e.stopPropagation();
+     }
+
+ return (<>
+   <div onClick={stateclick} id={props.id}>{props.name}</div>
+   {getcity && props.city.map( (ct,index) => <City name={ct.name}  id={`city${index+1}`}city ={ct.towns}></City>)}
+ </>); 
+}
+
 function App() {
-  return <div id="main"></div>;
+  return (
+    <div id="main">
+    {states.map( (state,index )=>  <States city={state.cities} id={`state${index+1}`} name={state.name}></States>  )}      
+    </div>
+  );
 }
 
 export default App;
+
